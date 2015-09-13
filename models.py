b4fedef87975fe5909628ac718597a34ca86f40e
hohsiangwu <hohsiangwu@gmail.com>
2015-09-12 20:54:01 -0700

    add transactions to Task and refactor enrich Diff

--- models.py
+++ models.py
@@ -18,29 +18,45 @@ class User(object):
 class Diff(object):
   def __init__(self, **fields):
     self.__dict__.update(fields)
+    self._commit_message = None
+
+  @property
+  def commit_message(self):
+    if not self._commit_message:
+      message = phab.differential.getcommitmessage(revision_id=self.id).response
+      self._commit_message = phab.differential.parsecommitmessage(corpus=message).fields
+    return self._commit_message
 
   def enrich(self):
-    msg = phab.differential.getcommitmessage(revision_id=self.id).response
-    self.__dict__.update(phab.differential.parsecommitmessage(corpus=msg).fields)
+    self.__dict__.update(self.commit_message)
     return self
 
   @classmethod
-  def all(cls, limit=1000):
+  def all(cls, limit=1000, enrich=False):
     # Warning: This method will take long time.
     diffs = [cls(**diff) for diff in phab.differential.query(limit=limit,
                                                              order='order-created',
                                                              status='status-closed')]
-    [diff.enrich() for diff in diffs]
+    if enrich:
+      [diff.enrich() for diff in diffs]
     return diffs
 
 
 class Task(object):
   def __init__(self, **fields):
     self.__dict__.update(fields)
+    self._transactions = None
 
   @classmethod
   def all(cls, limit=1000):
     return [cls(**task) for task in phab.maniphest.query(status='status-resolved', order='order-created', limit=limit).response.values()]
+
+  @property
+  def transactions(self):
+    if not self._transactions:
+      self._transactions = phab.maniphest.gettasktransactions(ids=[int(self.id)]).response[self.id]
+    return self._transactions
+
 
 class Project(object):
   def __init__(self, **fields):
